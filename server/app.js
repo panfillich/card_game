44a6025310972353bdebb64c87c56a8592b26320
panfillich <tarakan29@gmail.com>
2016-10-21 17:30:53 +0300

    Cocos and express

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 'use strict';
-
+var config  = require('./config');
 var express = require('express');
 var app = express();
 
+//Делаем файлы публичными
+//К ним теперь можно обратится site/js/somefile.js
+//app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(config.publicFolder));
+
+//Обработчики или middleware
+var myLogger = function (req, res, next) {
+    console.log(config.publicFolder);
+    next();
+};
+
+app.use(myLogger);
 // var initialisers = [
 //     require('./config/http'),
 //     require('./config/routing')
