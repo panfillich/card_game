ef0d21ede3bd6ce1f9d70688895da40a1c4d8378
panfillich <tarakan29@gmail.com>
2016-12-13 18:39:54 +0300

    sequelize migration

---
+++ database/migrations/20161213062415-unnamed-migration.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable(
+        'users',
+        {
+          user_id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          login: Sequelize.STRING(256),
+          email: Sequelize.STRING(256),
+          password: Sequelize.STRING(256),
+          token: Sequelize.STRING(256),
+          createdAt: {
+            type: Sequelize.DATE
+          },
+          updatedAt: {
+            type: Sequelize.DATE
+          }
+        },
+        {
+          engine: 'MYISAM',                     // default: 'InnoDB'
+          charset: 'utf8',
+          collate: 'utf8_general_ci'
+        }
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+      return queryInterface.dropTable('users');
+
+  }
+};
