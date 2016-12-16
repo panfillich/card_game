9578f90b4e61dd73dcf6be2de1329dc317c00bbe
panfillich <tarakan29@gmail.com>
2016-12-16 19:16:14 +0300

    sequelize migration

--- database/migrations/users.js
+++ database/migrations/users.js
@@ -1,6 +1,10 @@
+let list_users = [];
+let current_num = 0;
+let count_user = 10;
+
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.createTable(
+     queryInterface.createTable(
         'users',
         {
           user_id: {
@@ -12,23 +16,35 @@ module.exports = {
           email: Sequelize.STRING(256),
           password: Sequelize.STRING(256),
           token: Sequelize.STRING(256),
-          createdAt: {
+          created_at: {
             type: Sequelize.DATE
           },
-          updatedAt: {
+          updated_at: {
             type: Sequelize.DATE
           }
         },
         {
-          engine: 'MYISAM',                     // default: 'InnoDB'
+          engine: 'InnoDB',                     // default: 'InnoDB'
           charset: 'utf8',
           collate: 'utf8_general_ci'
         }
-    )
+    );
+
+    while (current_num<count_user) {
+      list_users.push({
+          login: 'user' + current_num,
+          email: 'user' + current_num + '@gmail.com',
+          password: '',
+          token: '',
+          created_at: new Date(),
+          updated_at: new Date()
+      })
+      current_num++;
+    }
+    queryInterface.bulkInsert('users', list_users);
   },
 
   down: function (queryInterface, Sequelize) {
       return queryInterface.dropTable('users');
-
   }
 };
