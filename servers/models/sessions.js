e4028522304f87c667eb1ce8258be6d71a765d8a
panfillich <tarakan29@gmail.com>
2017-03-28 01:06:56 +0300

    Redis

--- servers/models/sessions.js
+++ servers/models/sessions.js
@@ -22,22 +22,99 @@ class Sessions{
 
     createNewSession(param, callback){
         let token = Token.createForUser(param.email);
-        this._checkUserSession(param.userId, function (err, result) {
+
+        // создаем сессию-токен
+        this._createTokenSession(token.hash, (err) => {
             if(err){
-                callback(err, false);
-            } else if(result){
-               // используем готовый user:id (только поменяем ему token)
-
+                callback(err, null);
             } else {
-               // создаем новый user:id
+                // после создания токена проверяем сессию пользователя
+                this._checkUserSession(param.userId, (err, result) => {
+                    if (err) {
+                        callback(err, null);
+                    } else if (result) {
+                        // используем готовый user:id (только поменяем ему token, а старый удалим)
+                        this._chandeTokenInUserSession(param.userId, token.hash, function (err) {
+                            if(err){
+                                callback(err, null);
+                            } else {
+                                callback(null, token);
+                            }
+                        });
+                    } else {
+                        // создаем новый user:id
+                        this._createUserSession(param, function (err) {
+                            if(err){
+                                callback(err, null);
+                            } else {
+                                callback(null, token);
+                            }
+                        });
+                    }
+                });
+            }
 
+        });
+    }
+
+
+    // Создаем сессию-токен token:[token]
+    _createTokenSession(token_hash, callback){
+        let client = this.client;
+        const KEY = [TOKEN_PREFIX, param.userId].join(':');
+        client.multi([
+            ['set', KEY, token_hash],
+            ['expire', KEY, LIFETIME]//
+        ]).exec(function (err, res) {
+            if (err || res[0]!='OK') {
+                callback("Error in RAM: can't create token session")
+            } else {
+                callback(null);
             }
         });
     }
 
-    // Создаем пользовательскую сессию user:id
-    _createUserSession(){
+    // Создаем пользовательскую сессию user:[id]
+    _createUserSession(param, callback){
+        let client = this.client;
+        const KEY = [USER_PREFIX, param.userId].join(':');
 
+        let hmset = ['hmset', KEY];
+        for (let key in param) {
+            hmset.push(key);
+            hmset.push(fields[key]);
+        }
+        client.multi([
+            hmset,
+            ['expire', KEY, LIFETIME]//
+        ]).exec(function (err, res) {
+            if (err || !res[0]!='OK') {
+                callback("Error in RAM: can't create user session", null)
+            } else {
+                callback(null)
+            }
+        });
+    }
+
+    // Заменяем токен в пользовательской сессии а старый токен удаляем
+    _chandeTokenInUserSession(userId, token_hash, callback){
+        let client = this.client;
+        const USER_KEY = [USER_PREFIX, userId].join(':');
+
+        client.multi([
+            ['hget', USER_KEY, 'token'],
+            ['hset', USER_KEY, 'token', token_hash]//
+        ]).exec(function (err, res) {
+            if (err || !res[0]) {
+                callback("Error in RAM: can't change token in user session")
+            } else {
+                const OLD_TOKEN_KEY = [TOKEN_PREFIX, res[0]].join(':');
+                client.del(OLD_TOKEN_KEY);
+                callback(null);
+            }
+        });
+
+        const OLD_TOKEN_KEY = '';
     }
 
     // Проверям существует ли ключ user:id, если существует то добавляем ему время жизни
@@ -56,8 +133,30 @@ class Sessions{
         });
     }
 
-    createSession(){
+    // Получить всю сессию
+    getSession(token, callback){
+        let client = this.client;
+        const TOKEN_KEY = [TOKEN_PREFIX, token].join(':');
 
+        client.multi([
+            ['get', TOKEN_KEY],
+            ['expire', TOKEN_KEY, LIFETIME]
+        ]).exec(function (err, res) {
+            if (err) {
+                callback(err, null)
+            } else if(res[0]){
+                const USER_ID = res[0];
+                const USER_KEY = [USER_PREFIX, USER_ID].join(':');
+                client.multi([
+                    ['hgetall', USER_KEY],
+                    ['expire',  USER_KEY, LIFETIME]
+                ]).exec(function (err, res) {
+                    console.log(res[0]);
+                });
+            } else {
+                callback(null, null);
+            }
+        });
     }
 
 
